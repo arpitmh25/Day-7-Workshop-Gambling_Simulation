adba9bebe333f9edfb2e9419c0bc2da7cf52f7d1
arpitmh25 <95077786+arpitmh25@users.noreply.github.com>
2021-12-22 16:23:09 +0530

    Update GamblingSimulation.java
    Implemented use cases and perform program

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -1,18 +1,83 @@
-class GamblingSimulation {
-	public static final int InitialStake = 100;
-    public static final int BetAmount = 1;
-		public static void main(String[] args) {
-		double RandomNum = Math.random();
-        int RemainingAmount = InitialStake;
-        if (RandomNum>0.5)
-		{
-            RemainingAmount += BetAmount;
-            System.out.println("Gambler wins bet $"+RemainingAmount+ " After first Bet.");
+import java.util.*;
+public class GamblingSimulation {
+    /* Use case 1 */
+    public static final int initialStake =100;
+    public static final int betAmount =1;
+    public static final int noOfDays =20;
+
+    public static void main(String[] arg) {
+        double random_number = Math.random();
+        /* Use Case 2 */
+        int remainingStake = initialStake;
+        if (random_number>0.5)
+        {
+            remainingStake += betAmount;
+            System.out.println("The Gambler wins the bet and he has $"+remainingStake+ " with him after one bet.");
         }
-		else if (RandomNum<=0.5)
-		{
-            RandomNum -= BetAmount;
-            System.out.println("Gambler loses bet $"+RemainingAmount+ " Having amount after one bet.");
+        else
+        {
+            remainingStake -= betAmount;
+            System.out.println("The Gambler loses the bet and he has $"+remainingStake+ " with him after one bet.");
+        }
+
+        /* Use Case 3 */
+        double totalAmt = initialStake;
+        while ((totalAmt < 150) && (totalAmt > 50))
+        {
+
+            if (random_number > 0.5) {
+                totalAmt += betAmount;
+            } else 
+            {
+                totalAmt -= betAmount;
+            }
+        }
+        System.out.println("The gambler resigns for the day after he has $" + totalAmt + " with him.");
+
+        /* Use Case 4 */
+        double totalAmountForDay = 0;
+        double dailyAmount = 0;
+        for (int i = 1; i <= noOfDays; i++)
+        {
+            int numberOfBets = 0;
+            while ((dailyAmount < 50) && (dailyAmount > -50) && (numberOfBets < initialStake)) {
+                numberOfBets++;
+
+                if (random_number > 0.5) {
+                    dailyAmount += betAmount;
+                } else {
+                    dailyAmount -= betAmount;
+                }
+            }
+            totalAmountForDay += dailyAmount;
+        }
+        if (totalAmountForDay > 0) 
+        {
+            System.out.println("Total amount won in 20 days = " + totalAmountForDay);
         } 
+        else if (totalAmountForDay == 0) 
+        {
+            System.out.println("No net gain or loss in 20 days");
+        } 
+        else 
+        {
+            System.out.println("Total amount lost in last 20 days = " + Math.abs(totalAmountForDay));
+        }
+
+        /* Use Case 5  */
+        for(int j=1; j<=30;j++) {
+            if (dailyAmount > 0)
+            {
+                System.out.println("Amount won on day " + j + " = $" + dailyAmount);
+            } 
+            else if (dailyAmount == 0)
+            {
+                System.out.println("No net gain or loss on day " + j);
+            } 
+            else 
+            {
+                System.out.println("Amount lost on thatday " + j + " = $" + Math.abs(dailyAmount));
+            }
+        }
     }
 }
