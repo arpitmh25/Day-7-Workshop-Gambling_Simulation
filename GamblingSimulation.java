a55ea342c9099989ef8c5113b4e8c8fd8f430ae6
arpitmh25 <95077786+arpitmh25@users.noreply.github.com>
2021-12-22 08:48:43 +0530

    Update GamblingSimulation.java
    Implementing Use case 2 in Gambling simulation

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -1,7 +1,18 @@
 class GamblingSimulation {
-	public static void main(String[] args) {
 	public static final int InitialStake = 100;
     public static final int BetAmount = 1;
-	}
+		public static void main(String[] args) {
+		double RandomNum = Math.random();
+        int RemainingAmount = InitialStake;
+        if (RandomNum>0.5)
+		{
+            RemainingAmount += BetAmount;
+            System.out.println("Gambler wins bet $"+RemainingAmount+ " After first Bet.");
+        }
+		else if (RandomNum<=0.5)
+		{
+            RandomNum -= BetAmount;
+            System.out.println("Gambler loses bet $"+RemainingAmount+ " Having amount after one bet.");
+        } 
+    }
 }
-	 
